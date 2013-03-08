8ab6eac534b2b31475b84e680c6a013a8819293c
Herón Ordóñez <heronog@gmail.com>
2013-03-08 17:58:02 -0600

    Added a map page. and menu item, and open/closed script with the space api

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,6 +1,16 @@
-/* Author:
-
-*/
+jQuery(document).ready(function() {
+	jQuery.ajax({
+		'url': '/status.json',
+		success: function(data, textStatus, XHR) {
+		if(data.open) {
+			jQuery('#api_status').text('abierto').addClass('space_open').removeClass('space_closed');
+		} else {
+			jQuery('#api_status').text('cerrado').addClass('space_closed').removeClass('space_open');
+		}
+	}
+	});
+	
+});
 
 
 
