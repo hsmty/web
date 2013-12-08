ed5fb23f131c931a9a78a0d41cf61777dea326cc
R. Diaz de Leon <leon@elinter.net>
2013-12-07 22:45:33 -0600

    Added publish task for creating the static site

--- Rakefile.rb
+++ Rakefile.rb
@@ -7,3 +7,12 @@ task :serve do
   exec 'bundle exec jekyll serve --watch --drafts' if Process.fork.nil?
   Process.wait
 end
+
+task :publish do
+  Signal.trap :SIGINT do
+    exit
+  end
+
+  exec 'bundle exec sass _style/style.scss:css/style.min.css --style compressed'
+  exec 'bundle exec jekyll build'
+end
